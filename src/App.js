e0f2ec1353163b4734e4272cc397757795aad9ee
Kiddos <jeremiahdgage@outlook.com>
2022-10-04 19:04:49 -0500

    Fixes after delete (JeremiahDuane)

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 import Recipe from './Recipes/Recipe';
+import data from './JSON/Recipes.json';
 
 function App() {
   return (
@@ -7,9 +8,7 @@ function App() {
       <div className="cover-wrapper">
         <h1 className="cover">Chloe's Recipes</h1>
       </div>
-      <Recipe name="Cinnamon Milk"/>
-      <Recipe name=""/>
-      <Recipe name=""/>
+      {data.Recipes.map((recipe, index) => <Recipe key={index} name={recipe.name} ingredients={recipe.ingredients} description={recipe.description}/>)}
     </div>
   );
 }
--- src/Recipes/Recipe.js
+++ src/Recipes/Recipe.js
@@ -1,7 +1,17 @@
+import './Recipe.css';
+
 function Recipe(props) {
   return (
     <div className="Recipe">
-        <h2>{props.name}</h2>
+        <h2>
+          {props.name}
+        </h2>
+        <ul>
+          {props.ingredients.map((ingredient, index) => <li key={index}>{ingredient}</li>)}
+        </ul>
+        <p>
+          {props.description}
+        </p>
     </div>
   );
 }
